34c56c67a55f30322362ad18226ddb962f9119c6
Andrew Nesbitt <andrewnez@gmail.com>
2011-03-16 09:57:21 +0000

    Cache megafonzies to redis

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra'
 gem 'twitter'
 gem 'haml'
 gem 'yajl-ruby'
+gem 'redis'
 
 group :development do
   gem 'shotgun'
--- coolometer.rb
+++ coolometer.rb
@@ -1,6 +1,11 @@
 class Coolometer < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
+  configure do
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
+
   get '/' do
     haml :index
   end
@@ -17,27 +22,32 @@ class Coolometer < Sinatra::Base
 
   def calculate_coolness_for(username)
     @user = Twitter.user(username)
-    timeline = Twitter.user_timeline(username)
-    trends = Twitter.trends_weekly
-    @megafonzies = 0
+    if megafonzies = REDIS.get(username)
+      return @megafonzies = megafonzies.to_f
+    else
+      timeline = Twitter.user_timeline(username)
+      trends = Twitter.trends_weekly
+      @megafonzies = 0
 
-    # amount of time mentions recent trends
-    squished_timeline = timeline.map(&:text).join(' ')
-    buzzwords = trends.map{|day| day[1].map(&:name) }.flatten.uniq
-    buzzwords.each do |buzzword|
-      @megafonzies +=1 if squished_timeline.match(Regexp.new(buzzword,true))
+      # amount of time mentions recent trends
+      squished_timeline = timeline.map(&:text).join(' ')
+      buzzwords = trends.map{|day| day[1].map(&:name) }.flatten.uniq
+      buzzwords.each do |buzzword|
+        @megafonzies +=1 if squished_timeline.match(Regexp.new(buzzword,true))
+      end
+
+      # amount of retweets
+      timeline.each {|tweet| @megafonzies += tweet.retweet_count.to_i}
+
+      # followers
+      @megafonzies += @user.followers_count.to_f/@user.friends_count
+
+      # listed
+      @megafonzies += @user.listed_count.to_f/100
+
+      # how long you've been on twitter
+      @megafonzies += (Time.now - Time.parse(@user.created_at)).to_f/10000000
+      REDIS.set @user.screen_name, @megafonzies
     end
-
-    # amount of retweets
-    timeline.each {|tweet| @megafonzies += tweet.retweet_count.to_i}
-
-    # followers
-    @megafonzies += @user.followers_count.to_f/@user.friends_count
-
-    # listed
-    @megafonzies += @user.listed_count.to_f/100
-
-    # how long you've been on twitter
-    @megafonzies += (Time.now - Time.parse(@user.created_at)).to_f/10000000
   end
 end
